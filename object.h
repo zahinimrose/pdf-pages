c90a487dbdc91dd407ee561172cffe0c9890c173
= <=>
2024-07-30 18:12:22 +0600

    Implemet stream and num object parsing

--- object.h
+++ object.h
@@ -6,6 +6,7 @@
 #include "lexer.h"
 
 
+
 typedef std::vector<char> Data;
 typedef int Idx;
 
@@ -24,6 +25,14 @@ inline void consume_white_space(const Data& data, Idx& i) {
     while(is_white_space(data[i])) i++;
 }
 
+
+void print_tok(Data tok) {
+    for (char ch: tok) {
+        cout << ch;
+    }
+    cout << "\n";
+}
+
 class Object {
     bool indirect;
     static unordered_map<int, Object>& table;
@@ -33,6 +42,7 @@ public:
     
 };
 
+unordered_map<int, Object*> table;
 Object* direct_parse(const Data& data, Idx& i);
 
 class Reference : public Object {
@@ -109,6 +119,7 @@ public:
     }
 
     Data serialize() const {
+
         Data d;
         d.push_back('<');
         d.push_back('<');
@@ -129,8 +140,73 @@ public:
 };
 
 class Stream_object: public Object {
+public:
     Dict_object dict;
     Data data;
+
+    static Stream_object parse(const Data& data, Idx& i, Dict_object dict) {
+        Lexer l(data, i);
+        auto tok = l.read_next_tok();
+        if(!Lexer::equalsString(tok, "stream")) {
+            std::cout << "Cannot parse non streams" << std::endl;
+            exit(1);
+        }
+
+        Name_object n;
+        n.name = "Length";
+        if (!dict.map.count(n)) {
+            std::cout << "Stream dict must have Length Field" <<std::endl;
+        }
+        int stream_length = stoi(Lexer::toString(dict.map[n]->serialize()));
+        cout << "Length: " << stream_length << std::endl;
+
+        Stream_object obj;
+        obj.dict = dict;
+
+        if(data[i] == 10) {
+            i++;
+        }
+        else if(data[i] == 13 && data[i + 1] == 10) {
+            i+=2;
+        }
+        else {
+            cout << "ERROR: Invalid EOF after stream keyword";
+            exit(1);
+        }
+        for(int j = 0; j < stream_length; j++) {
+            obj.data.push_back(data[i]);
+            i++;
+        }
+
+        cout<< obj.data[0] << "\n";
+        Lexer m(data, i);
+        tok = m.read_next_tok();
+
+        if(!Lexer::equalsString(tok, "endstream")) {
+            std::cout << "Stream data must end with endstream" << std::endl;
+            exit(1);
+        }
+
+        return obj;
+
+    }
+
+    Data serialize() const {
+        auto d = dict.serialize();
+        d.push_back(' ');
+
+        string str = "stream";
+        d.push_back(' ');
+        d.insert(d.end(), str.begin(), str.end());
+        d.push_back(10);
+        d.insert(d.end(), data.begin(), data.end());
+        d.push_back(10);
+        str = "endstream";
+        d.insert(d.end(), str.begin(), str.end());
+        d.push_back(10);
+
+        return d;
+    }
 };
 
 class String_object : public Object {
@@ -173,23 +249,73 @@ public:
     }
 };
 
+class Num_object : public Object {
+public:
+    Data num;
+    static Num_object parse(const Data& data, Idx& i) {
+        Lexer l(data, i);
+        auto tok = l.read_next_tok();
+        if(!isdigit(tok[0])) {
+            cout << "Not a numeric object" << std::endl;
+            exit(1);
+        }
+
+        Num_object num_obj;
+        num_obj.num = tok;
+        return num_obj;
+    }
+
+    Data serialize() const {
+        
+        return num;
+    }
+};
+
 Object* direct_parse(const Data& data, Idx& i) {
         Lexer l(data, i);
         auto tok = l.peek_next_tok();
         if (Lexer::equalsString(tok, "<<")) {
             Dict_object dict = Dict_object::parse(data, i);
+            tok = l.peek_next_tok();
+
+            if(Lexer::equalsString(tok, "stream")) {
+                
+
+                auto stream = Stream_object::parse(data, i, dict);
+                auto ret = new Stream_object;
+                *ret = stream;
+
+                return ret;
+            }
+
+
             auto ret = new Dict_object;
             *ret = dict;
             return ret;
         }
         else if(isdigit(tok[0])) {
-            cout << "Error: Not implemented";
-            exit(1);
-            // tok = l.read_next_tok();
+            auto next = l.peek_next_next_tok();
+            if(isdigit(next[0])) {
+                cout << "Parsing references not implemented";
+                exit(1);
+            }
+            else {
+                Num_object num = Num_object::parse(data, i);
+                auto ret = new Num_object;
+                *ret = num;
+
+                return ret;
+            }
         }
         else if(Lexer::equalsString(tok, "(")) {
             String_object s = String_object::parse(data, i);
             auto ret = new String_object;
+            *ret = s;
+            return ret;
+        }
+        else if(tok[0] == '/') {
+            Name_object s = Name_object::parse(data, i);
+            auto ret = new Name_object;
             *ret = s;
             return ret;
         }
@@ -210,5 +336,15 @@ Object* indirect_parse(const Data& data, Idx& i) {
             cout << "ERROR: Indirect reference must end with endobj";
         }
 
+        table[obj_num] = obj;
+        cout << "Parsed obj num " << obj_num << std::endl;
         return obj;
+}
+
+void parse_indirect_objects(const Data& data, Idx& i) {
+    Lexer l(data, i);
+    auto tok = l.peek_next_tok();
+    while(!Lexer::equalsString(tok, "xref")) {
+        indirect_parse(data, i);
     }
+}
