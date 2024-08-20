807323dd6115557aa2729b6c94d7868df3fba586
= <=>
2024-08-20 21:49:47 +0600

    Add merge count and some logging

--- merge.cpp
+++ merge.cpp
@@ -7,6 +7,7 @@ string default_output = "merged.pdf";
 int main() {
     Pdf merged;
 
+    int count = 0;
     namespace fs = std::filesystem;
     for (const auto& entry : fs::directory_iterator(".")) {
         if (entry.is_regular_file() && entry.path().extension() == ".pdf") {
@@ -14,11 +15,16 @@ int main() {
             cout << "Merging " << name << "\n";
             Pdf p(name);
             merged.merge(p);
-
+            count++;
         }
     }
 
+    if(count == 0) {
+        cout << "No pdf files found in current directory" << endl;
+        exit(1);
+    }
+
     merged.output(default_output);
-    cout << "Merging succesfully complete!" << endl;
+    cout << "Successfully merged " << count << " files!" << endl;
     cout << "Output: " << default_output << endl;
 }
